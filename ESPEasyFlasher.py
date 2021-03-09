b70ff18a8840bf018b7f91073ece1d0141eefc4b
hredan <hredan@sourcecode3d.de>
2021-03-09 13:05:36 +0100

    add a Module for Esptool communication (EsptoolCom.py) add a config json (ESPEasyFlasherConfig.json) add erase functionality

--- ESPEasyFlasher.py
+++ ESPEasyFlasher.py
@@ -1,23 +1,51 @@
+'''
+  ESPEasyFlasher.py is a Simple GUI for esptool.py by espressif
+  (https://github.com/espressif/esptool)
+  
+  It is also written in Python and is using Tkinter as GUI framework.
+
+  Targets:
+    * Using should be simple, also for people without experience of command line tools
+    * It can be easily configured by experienced people and user of the esptool.py
+    * It can be used in two modes (Developer/User)
+    * It is platform independent same as esptool.py 
+  
+  Copyright (C) 2021  André Herrmann
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 import sys
-sys.path.insert(0, 'c:/users/andre/appdata/roaming/python/python39/site-packages/')
-sys.path.insert(0, 'c:/users/andre/appdata/roaming/python/python39/site-packages/pil/')
-
 import threading
-
-import esptool
+import re
+import json
 
 import tkinter as tk
 from tkinter import ttk
 
 from serial.tools.list_ports import comports
 
-import re
+import EsptoolCom
+
+from io import StringIO
+
+# With Flag developerMode you can expand the use cases in the GUI with read and erase flash
+
 
 class IORedirector(object):
     '''A general class for redirecting I/O to this Text widget.'''
     def __init__(self,text_area, progressBar):
         self.text_area = text_area
         self.progressBar = progressBar
+
 class StdoutRedirector(IORedirector):
     '''A class for redirecting stdout to this Text widget.'''
     
@@ -50,18 +78,15 @@ class StdoutRedirector(IORedirector):
     def isatty(self):
         return False
 
-def esptoolReadFlash(comPort):
-    #for testing a short byte array
-    command = ['--port', comPort, '--baud', '460800', 'read_flash', '0', '0x010000', 'flash_contents.bin']
-
-    #command = ['--port', comPort, '--baud', '460800', 'read_flash', '0', '0x400000', 'flash_contents.bin']    
-    print('Using command %s' % ' '.join(command))
-    esptool.main(command)
-
 class App:
     def __init__(self, master):
-        developer = True
+        self.developerMode = True
+        
+        # read config from json file
+        configStr = self.readConfig()
+
         master.title("ESPEasyFlasher2.0")
+        master.resizable(0, 0)        
         
         frame = ttk.Frame(master)
         frame.pack()
@@ -106,7 +131,7 @@ class App:
         tk.Grid.columnconfigure(self.writeGroup, 1, weight=2)
 
         # read Group
-        if (developer):
+        if (self.developerMode):
             rowPosFrame += 1
             self.readGroup = tk.LabelFrame(frame, text='ReadFlash')
             self.readGroup.grid(column=0, row=rowPosFrame, sticky="EW", padx=5, pady=5)
@@ -115,7 +140,8 @@ class App:
             self.labelReadBin = tk.Label(self.readGroup, text="Flash write to: ")
             self.labelReadBin.grid(column=0, row=rowPosRead, sticky="W")
 
-            self.entryFileName = tk.Entry(self.readGroup, width=20, textvariable="flash_4MB")
+            defaultText = tk.StringVar(self.readGroup, value="filename_read_flash")
+            self.entryFileName = tk.Entry(self.readGroup, width=20, textvariable=defaultText)
             self.entryFileName.grid(column=1, row=rowPosRead, sticky="EW")
 
             self.fileExtension = tk.Label(self.readGroup, text=".bin")
@@ -128,6 +154,16 @@ class App:
             tk.Grid.columnconfigure(self.readGroup, 0, weight=1)
             tk.Grid.columnconfigure(self.readGroup, 1, weight=2)
             tk.Grid.columnconfigure(self.readGroup, 2, weight=1)
+
+            rowPosFrame += 1
+            self.eraseGroup = tk.LabelFrame(frame, text='EraseFlash')
+            self.eraseGroup.grid(column=0, row=rowPosFrame, sticky="EW", padx=5, pady=5)
+
+            rowPosErase = 0
+            self.readBtn = tk.Button(self.eraseGroup, text="EraseFlash", command=self.eraseFlash)
+            self.readBtn.grid(column=0, row=rowPosRead, sticky="EW", padx=3, pady=3)
+
+            tk.Grid.columnconfigure(self.eraseGroup, 0, weight=1)
 
         # Textbox Logging
         rowPosFrame += 1
@@ -148,29 +184,43 @@ class App:
         self.progress.grid(column=0, row=rowPosFrame, columnspan = 2, sticky="EW", padx=5, pady=5)
 
         sys.stdout = StdoutRedirector(self.text_box, self.progress)
+
+        # write config string
+        self.text_box.insert(tk.END, configStr)
+        
         # scan com ports
         self.comScan()
-    
+
+    def eraseFlash(self):
+        print("### Erase Flash ###")
+        comPort = self.comboComPort.get()
+        if (comPort == ""):
+            print("Error: select a Serial Com Port before you can start read flash!")
+        else:
+            x = threading.Thread(target=EsptoolCom.esptoolEraseFlash, args=(comPort,))
+            x.start()
+
     def readFlash(self):
         self.progress["value"] = 0
         self.progress["maximum"] = 100
 
         print("### Read Flash ###")
         comPort = self.comboComPort.get()
+        filename = self.entryFileName.get()
         if (comPort == ""):
             print("Error: select a Serial Com Port before you can start read flash!")
+        elif (filename == ""):
+            print("Error: before you can read flash, define a filename")
         else:
-            x = threading.Thread(target=esptoolReadFlash, args=(comPort,))
+            x = threading.Thread(target=EsptoolCom.esptoolReadFlash, args=(comPort, filename,))
             x.start()
             
 
-    
-    
     def comScan(self):
         comInfo = self.getComInfo()
         if (len(comInfo["comlist"]) > 0):
-            self.comboComPort.configure(values = comInfo["comlist"])
-        
+            self.comboComPort["values"] = comInfo["comlist"]            
+            
             if(comInfo["defaultCom"] != ""):
                 self.comboComPort.current(comInfo["comlist"].index(comInfo["defaultCom"]))
             else:
@@ -187,6 +237,7 @@ class App:
         if (len(comlist) > 0):
             defaultCom = comlist[0].name
 
+        isFundUsbSerial = False
         for com in comlist:
             # print(com.name)
             print("*" + com.description)
@@ -194,9 +245,39 @@ class App:
             if(com.description.lower().find("usb-serial") != -1 ):
                 defaultCom = com.name
                 print("Found: "+defaultCom)
+                isFundUsbSerial = True
+
+        if (isFundUsbSerial != True):
+            print("Warning: could not find a usb-serial device, connect your device and scan again!")
 
         return {"comlist": clist, "defaultCom": defaultCom}
 
+    def readConfig(self):
+        strIo = StringIO()
+        strIo.write("### Read Config ###\n")
+        try:
+            with open('ESPEasyFlasherConfig.json') as json_file:
+                data = json.load(json_file)
+                
+                self.developerMode = data['devMode']
+                strIo.writelines(f"dev mode is: {data['devMode']}\n")
+                
+                EsptoolCom.baudRate = data['baudRate']
+                strIo.write(f"set baud rate to: {data['baudRate']}\n")
+                
+                EsptoolCom.readStart = data['readStart']
+                strIo.write(f"set read start to: {data['readStart']}\n")
+                
+                EsptoolCom.readSize = data['readSize']
+                strIo.write(f"set read size to: {data['readSize']}\n")
+                
+                EsptoolCom.writeStart = data['writeStart']
+                strIo.write(f"set write start to: {data['writeStart']}\n")
+
+        except EnvironmentError as err:
+            strIo.writelines(f"Error could not read config, default values will be used: {err}\n")
+        
+        return strIo.getvalue()
 
 if __name__ == "__main__":
 
---
+++ EsptoolCom.py
@@ -0,0 +1,44 @@
+'''
+  EsptoolCom.py is a simple python interface to esptool.py (https://github.com/espressif/esptool).
+  It is used by ESPEasyFlasher.py to write, read, and erase flash of esp microcontroller.
+  https://github.com/hredan/ESPEASYFLASHER_2.0
+  
+  Copyright (C) 2021  André Herrmann (hredan)
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+import esptool
+
+# Config variables can be changed by EspEasyFlasherConfig.json
+baudRate = '460800'
+
+readStart = '0'
+readSize = '0x400000'
+
+writeStart = '0x00000'
+
+def esptoolReadFlash(comPort, filename):
+    #for testing a short byte array
+    command = ['--port', comPort, '--baud', baudRate, 'read_flash', readStart, readSize, filename]
+    #command = ['--port', comPort, '--baud', '460800', 'read_flash', '0', '0x400000', 'flash_contents.bin']      
+    startEsptool(command)
+
+def esptoolWriteFlash(comPort, filename):
+    command = ['--port', comPort, '--baud', baudRate, 'write_flash', writeStart, filename]
+    startEsptool(command)
+
+def esptoolEraseFlash(comPort):
+    command = ['--port', comPort, 'erase_flash']
+    startEsptool(command)
+
+def startEsptool(command):
+    print('Using command %s' % ' '.join(command))
+    esptool.main(command)
