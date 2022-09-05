accd4a9084233cfd5ccefc24825919f4c7a4bd00
hredan <hredan@sourcecode3d.de>
2022-09-05 16:44:23 +0200

    add option for sha, tag, name

--- Scripts/build_info.py
+++ Scripts/build_info.py
@@ -9,11 +9,19 @@ HELP="""Paramter:
 -t\tTAG_NAME
 e.g. python ./build_info.py -s \$GITHUB_SHA -r \$GITHUB_REPOSITORY"""
 
-def create_info_file():
+def create_info_file(repo_name, tag_name, sha):
+    print(f"{repo_name}, {tag_name} ,{sha}")
     installed_packages = pkg_resources.working_set
     installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
 
     with open('build_info.txt', 'w') as f:
+        if(repo_name):
+          f.write('Repository URL:  www.github.com/' + repo_name + '\n')
+        if(sha):
+          f.write('GITHUB_SHA:      ' + sha + '\n')
+        if(tag_name):
+          f.write('Release version: ' + tag_name + '\n')
+
         f.write('OS: ' + platform.system() + platform.release() + '\n')
         f.write('Pyhton Version: ' + sys.version + '\n')
         f.write('PIP list: \n')
@@ -26,13 +34,25 @@ def main(argv):
     sha = ''
 
     try:
-      opts, args = getopt.getopt(argv,"hsrt",["ifile=","ofile="])
-    except getopt.GetoptError:
+      opts, args = getopt.getopt(argv,"hs:r:t:")
+    except getopt.GetoptError as err:
+      print("Error:")
+      print(err)
+      print("use:")
       print(HELP)
       sys.exit(2)
+    print(opts)
     for opt, arg in opts:
       if opt == '-h':
          print(HELP)
+         sys.exit(0)
+      elif opt == "-r":
+        repo_name = arg
+      elif opt == "-t":
+        tag_name = arg
+      elif opt == "-s":
+        sha = arg
+    create_info_file(repo_name, tag_name, sha)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
