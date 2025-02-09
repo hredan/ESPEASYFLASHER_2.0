a83217097b25c6a3e536d904dd70e02ac3b4ed08
hredan <hredan@sourcecode3d.de>
2025-02-09 07:19:43 +0100

    test git name for all

--- Scripts/build_linux_arm.sh
+++ Scripts/build_linux_arm.sh
@@ -8,7 +8,7 @@ python3 -m venv /home/venv
 source /home/venv/bin/activate
 
 pip install -r requirements.txt
-python ./Scripts/build_info.py -s $GIT_HASH -r $GIT_URL
+python ./Scripts/build_info.py -s $GIT_HASH -r $GIT_URL -n $GIT_NAME
 python -m PyInstaller -F --collect-data esptool --add-data "./LogoEasyFlash.png:." --add-data "build_info.txt:." --name ESPEasyFlasher espeasyflasher.py
 cp ./ESPEasyFlasherConfig.json ./dist
 cp ./build_info.txt ./dist
--- Scripts/build_macOS.sh
+++ Scripts/build_macOS.sh
@@ -14,7 +14,14 @@ else
     GIT_HASH=$(git rev-parse HEAD)
 fi
 
-python3 ./Scripts/build_info.py -s $GIT_HASH -r $GIT_URL
+if [ -n "$GITHUB_REF_NAME" ]
+then
+    GIT_NAME=$GITHUB_REF_NAME
+else
+    GIT_NAME=$(git rev-parse --abbrev-ref HEAD)
+fi
+
+python3 ./Scripts/build_info.py -s $GIT_HASH -r $GIT_URL -n $GIT_NAME
 python3 -m PyInstaller -F --windowed --collect-data esptool --add-data "./LogoEasyFlash.png:." --add-data "build_info.txt:." --icon "./resource/macOs/SleepUino.icns" --name ESPEasyFlasher espeasyflasher.py
 cp ./ESPEasyFlasherConfig.json ./dist/ESPEasyFlasher.app/Contents/MacOS
 cp ./build_info.txt ./dist/ESPEasyFlasher.app/Contents/MacOS
