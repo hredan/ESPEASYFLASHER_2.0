894296a76591e0fa3073f3d7f3d778cb96deb32d
hredan <hredan@sourcecode3d.de>
2025-02-09 07:10:51 +0100

    test linux64

--- Scripts/build_linux.sh
+++ Scripts/build_linux.sh
@@ -15,8 +15,15 @@ else
     GIT_HASH=$(git rev-parse HEAD)
 fi
 
+if [ -n "$GITHUB_REF_NAME" ]
+then
+    GIT_NAME=$GITHUB_REF_NAME
+else
+    GIT_NAME=$(git rev-parse --abbrev-ref HEAD)
+fi
+
 pip install -r requirements.txt
-python ./Scripts/build_info.py -s $GIT_HASH -r $GIT_URL
+python ./Scripts/build_info.py -s $GIT_HASH -r $GIT_URL -n $GIT_NAME
 python -m PyInstaller -F --collect-data esptool --add-data "./LogoEasyFlash.png:." --add-data "build_info.txt:." --name ESPEasyFlasher espeasyflasher.py
 cp ./ESPEasyFlasherConfig.json ./dist
 cp ./build_info.txt ./dist
